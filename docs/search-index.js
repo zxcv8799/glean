ec2a1f79a80f2b3b7db5a81c0e10638c1cdf21d5
CircleCI docs-deploy job <jrediger@mozilla.com>
2019-03-27 17:52:34 +0000

    [skip ci] Updates

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,4 +1,4 @@
 var N = null;var searchIndex = {};
-searchIndex["glean_core"]={"doc":"","items":[],"paths":[]};
-searchIndex["glean_ffi"]={"doc":"","items":[],"paths":[]};
+searchIndex["glean_core"]={"doc":"","items":[[5,"increment","glean_core","",N,[[],["u64"]]]],"paths":[]};
+searchIndex["glean_ffi"]={"doc":"","items":[[5,"increment","glean_ffi","",N,[[],["u64"]]]],"paths":[]};
 initSearch(searchIndex);addSearchOptions(searchIndex);
