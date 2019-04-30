dcc8c9889c65148a393d4e5a631ca7dd0680f2c8
CircleCI docs-deploy job <jrediger@mozilla.com>
2019-04-30 12:34:00 +0000

    [skip ci] Updates

--- docs/search-index.js
+++ docs/search-index.js
@@ -1,4 +1,5 @@
-var N = null;var searchIndex = {};
-searchIndex["glean_core"]={"doc":"","items":[[5,"increment","glean_core","",N,[[],["u64"]]]],"paths":[]};
-searchIndex["glean_ffi"]={"doc":"","items":[[5,"increment","glean_ffi","",N,[[],["u64"]]]],"paths":[]};
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=["glean_core","glean_ffi","increment"];
+searchIndex[R[1]]={"doc":E,"i":[[5,R[2],R[1],E,N,[[],["u64"]]]],"p":[]};
+searchIndex[R[0]]={"doc":E,"i":[[5,R[2],R[0],E,N,[[],["u64"]]]],"p":[]};
 initSearch(searchIndex);addSearchOptions(searchIndex);
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["glean_core"] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex["glean_ffi"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
